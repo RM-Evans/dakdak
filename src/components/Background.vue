f11547fd015050f77d3e9a8ec00f9731987722a3
m evans <evansrichardmichael@gmail.com>
2026-09-04 00:47:23 -0600

    change intention

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -5,7 +5,7 @@ const props = defineProps({
   catClicked: Boolean
 })
 
-const colors = ['#FFF55E', '#35E871', '#34C2AA', '#671596']
+const colors = [ '#34C2AA', '#FFF55E', '#35E871', '#671596',]
 const currentColorIndex = ref(0)
 // const bgColor = ref(colors[currentColorIndex.value])
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,14 +10,21 @@ const count = ref(0)
 </script>
 
 <template>
-<h1 :class="{ light: props.catClicked, dark: !props.catClicked }">
-  click on pearl
-</h1>
+  <div class="bapper">
+ <h1 :class="{ cyberText: props.catClicked, hideCyberText: !props.catClicked }">
+hang in there
+ </h1>
+  <h1 :class="{ light: props.catClicked, dark: !props.catClicked }">
+    {{ props.msg }}
+    </h1>
+
+  </div>
 </template>
 
 <style scoped>
 .dark {
   color: #ffffff;
+  DROP-shadow: 0 10PX 20px rgba(33, 33, 33, 0.5);
  
 }
 .light {
@@ -27,8 +34,22 @@ const count = ref(0)
 h1{
 
 font-size: 30px;
-margin-bottom: 150%;
+
   transition: color 0.3s ease;
+}
+.cyberText {
+color: #2ef038;
+text-shadow: rgba(1, 29, 12, 0.3) 2px 2px 2px;
+}
+.hideCyberText {
+  visibility: hidden;
+}
+.bapper {
+  position: fixed;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
 }
 </style>
 const props = 
